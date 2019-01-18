1249beca1d5b3ce753929cf07906ac950c9067c1
DonMahallem <donmahallem@users.noreply.github.com>
2019-01-18 20:18:48 +0100

    limit file preview component test

---
+++ src/app/analyze/upload/file-preview.component.spec.ts
@@ -0,0 +1,38 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+import { GoogleAuthService } from 'ng-gapi';
+import { Observable, from } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { FilePreviewComponent } from './file-preview.component';
+
+@Injectable()
+class gUserService {
+    public isSignedInObservable(): Observable<boolean> {
+        return from([true, false, true]);
+    }
+}
+
+describe('FilePreviewComponent', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+                MatButtonModule,
+                MatToolbarModule
+            ],
+            declarations: [
+                FilePreviewComponent
+            ],
+            providers: [
+            ]
+        }).compileComponents();
+    }));
+
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(FilePreviewComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
+
+});
