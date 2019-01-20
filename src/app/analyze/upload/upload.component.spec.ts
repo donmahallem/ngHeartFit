1114e742e882afbd6881e5b34450e32f0481316a
DonMahallem <donmahallem@users.noreply.github.com>
2019-01-20 01:08:30 +0100

    fix tests again

--- src/app/analyze/upload/upload.component.spec.ts
+++ src/app/analyze/upload/upload.component.spec.ts
@@ -5,16 +5,37 @@ import { Observable, from } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { UploadComponent } from './upload.component';
 import { FilePreviewComponent } from './file-preview.component';
-import { UploadDataService } from '../services';
+import { UploadDataService, UploadFile } from '../services';
 
+import * as sinon from "sinon";
 
 @Injectable()
-class testUploadDataService {
-    public isSignedInObservable(): Observable<boolean> {
-        return from([true, false, true]);
+class testUploadDataService2 {
+
+    public get uploadedFiles(): UploadFile[] {
+        return [];
+    }
+    public update(): void {
+    }
+
+    public set uploadedFiles(files: UploadFile[]) {
+    }
+
+    public get uploadedFilesObservable(): Observable<UploadFile[]> {
+        return null;
+    }
+
+    public get uploadableFilesObservable(): Observable<boolean> {
+        return null;
+    }
+
+    public addUploadFile(f: UploadFile): void {
+    }
+
+    public clear(): void {
     }
 }
-
+let sandbox;
 describe('UploadComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -29,15 +50,39 @@ describe('UploadComponent', () => {
                 FilePreviewComponent
             ],
             providers: [
-                { provide: UploadDataService, useValue: testUploadDataService }
+                { provide: UploadDataService, useValue: new testUploadDataService2() }
             ]
         }).compileComponents();
     }));
+
+    beforeEach(() => {
+        //testUploadDataService.
+    });
 
     it('should create the app', () => {
         const fixture = TestBed.createComponent(UploadComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     });
-
+    beforeAll(() => { sandbox = sinon.sandbox.create(); })
+    afterEach(() => { sandbox.restore(); })
+    describe('uploadFiles()', () => {
+        it('should test', () => {
+            const fixture = TestBed.createComponent(UploadComponent);
+            const app: UploadComponent = fixture.debugElement.componentInstance;
+            expect(app).toBeTruthy();
+            let service = fixture.debugElement.injector.get(UploadDataService);
+            const stub: sinon.SinonStub = sandbox.stub(service, 'uploadedFiles');
+            const testData: any[] = [{
+                'test': 'data1',
+                'test2': 1234
+            }, {
+                'test': 'data6',
+                'test2': 1234436
+            }]
+            stub.get(() => { return testData });
+            expect(app.uploadFiles).toEqual(testData);
+            stub.restore();
+        });
+    });
 });
