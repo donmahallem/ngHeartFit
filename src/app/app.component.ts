7902a71f8880f99828e70813d2671868017e2def
DonMahallem <donmahallem@users.noreply.github.com>
2019-01-31 08:30:50 +0100

    removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,4 @@
 import { Component, OnInit, NgZone } from '@angular/core';
-import { GapiService } from './service/gapi.service';
-/**
- * Listener called when user completes auth flow. If the currentApiRequest
- * variable is set, then the user was prompted to authorize the application
- * before the request executed. In that case, proceed with that API request.
- */
-function updateSigninStatus(isSignedIn) {
-    console.log("sigin", isSignedIn);
-}
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.pug',
@@ -16,7 +7,4 @@ function updateSigninStatus(isSignedIn) {
 export class AppComponent {
     private _title: string = "app title";
 
-    constructor(private a: GapiService) {
-        a.getDataSources().subscribe(console.log, console.error);
-    }
 }
