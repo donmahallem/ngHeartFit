04bb27754e8f0e3f7047f44b305f952b87717131
DonMahallem <donmahallem@users.noreply.github.com>
2019-02-03 20:26:11 +0100

    added api endpoints

--- src/app/service/gapi.service.ts
+++ src/app/service/gapi.service.ts
@@ -44,4 +44,32 @@ export class GapiService {
         return this.gapiAuth.authRequest(this.http.post<any>("/api/google/fit/bodymetrics", data));
     }
 
+    /**
+     * 
+     * @param from unix timestamp in seconds since epoch
+     * @param to unix timestamp in seconds since epoch
+     */
+    public getWeight(from: moment.Moment, to: moment.Moment): Observable<any> {
+        const reqParams: HttpParams = new HttpParams();
+        reqParams.set('from', from.unix().toString());
+        reqParams.set('to', to.unix().toString());
+        return this.gapiAuth.authRequest(this.http.get<any>("/api/google/fit/weight", {
+            params: reqParams
+        }));
+    }
+    /**
+     * 
+     * @param from unix timestamp in seconds since epoch
+     * @param to unix timestamp in seconds since epoch
+     * @param bucketsize in seconds
+     */
+    public getWeightBucket(from: moment.Moment, to: moment.Moment, bucketsize: number): Observable<any> {
+        const reqParams: HttpParams = new HttpParams();
+        reqParams.set('from', from.unix().toString());
+        reqParams.set('to', to.unix().toString());
+        reqParams.set('bucketsize', bucketsize.toString());
+        return this.gapiAuth.authRequest(this.http.get<any>("/api/google/fit/weight", {
+            params: reqParams
+        }));
+    }
 }
