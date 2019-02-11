26c5d9d2d13454d2d218c4d8121479a1fad9b489
DonMahallem <donmahallem@users.noreply.github.com>
2019-02-11 18:19:34 +0100

    added

--- src/app/fit/session-detail/session-detail.component.ts
+++ src/app/fit/session-detail/session-detail.component.ts
@@ -12,23 +12,23 @@ import { Subscription } from 'rxjs';
 import { GapiUserService } from 'src/app/service/gapi-user.service';
 import { FitApiService } from 'src/app/service/fit-api.service';
 import { Router } from '@angular/router';
-import { DataSourceListResponse, DataSourceInformation } from 'src/app/service/fit-api-modals';
+import { DataSourceListResponse, DataSourceInformation, FitSession } from 'src/app/service/fit-api-modals';
 @Component({
     selector: 'session-detail',
     templateUrl: './session-detail.component.pug',
     styleUrls: ['./session-detail.component.scss']
 })
 export class SessionDetailComponent implements OnDestroy, AfterViewInit {
-    private mDataSource: DataSourceInformation;
+    private mDataSource: FitSession;
     constructor(private zone: NgZone, private nggapi: FitApiService) {
     }
 
     @Input('session')
-    public set dataSource(source: DataSourceInformation) {
+    public set dataSource(source: FitSession) {
         this.mDataSource = source;
     }
 
-    public get dataSource(): DataSourceInformation {
+    public get dataSource(): FitSession {
         return this.mDataSource;
     }
 
