a7903bce5bbd1955454fc573eb19b4eea9be4db0
DonMahallem <donmahallem@users.noreply.github.com>
2019-02-28 12:04:15 +0100

    a lot of tests added for file-preview.component

--- src/app/polar/upload/file-preview.component.spec.ts
+++ src/app/polar/upload/file-preview.component.spec.ts
@@ -1,17 +1,31 @@
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { MatButtonModule, MatToolbarModule, MatCheckboxModule, MatSlideToggle, MatSlideToggleModule } from '@angular/material';
-import { Observable, from } from 'rxjs';
-import { Injectable, Component, DebugElement } from '@angular/core';
+import {
+    TestBed,
+    async,
+    ComponentFixture,
+    fakeAsync
+} from '@angular/core/testing';
+import { MatCheckboxChange } from '@angular/material';
+import {
+    Injectable,
+    Component,
+    DebugElement,
+    Output,
+    EventEmitter,
+    Input
+} from '@angular/core';
 import { FilePreviewComponent } from './file-preview.component';
-import { UploadDataService, UploadFile, UploadFileType } from '../services';
+import {
+    UploadDataService,
+    UploadFile,
+    UploadFileType
+} from '../services';
 
 import * as sinon from 'sinon';
 import { By } from '@angular/platform-browser';
 @Injectable()
-class testUploadDataService {
-    public isSignedInObservable(): Observable<boolean> {
-        return from([true, false, true]);
+class TestUploadDataService {
+    public update(): void {
+
     }
 }
 
@@ -22,6 +36,23 @@ class testUploadDataService {
 })
 class TestParentComponent {
     public testFile: UploadFile;
+}
+@Component({
+    // tslint:disable-next-line
+    selector: 'mat-slide-toggle',
+    template: '<div></div>'
+})
+class TestSlideToggleComponent {
+    @Output()
+    public change: EventEmitter<MatCheckboxChange> = new EventEmitter();
+
+    @Input()
+    public checked: boolean;
+
+    @Input()
+    public disabled: boolean;
+    @Input()
+    public labelPosition: string;
 }
 
 describe('app/polar/upload/file-preview.component', () => {
@@ -76,14 +107,14 @@ describe('app/polar/upload/file-preview.component', () => {
         beforeEach(async(() => {
             TestBed.configureTestingModule({
                 imports: [
-                    MatSlideToggleModule
                 ],
                 declarations: [
                     FilePreviewComponent,
-                    TestParentComponent
+                    TestParentComponent,
+                    TestSlideToggleComponent
                 ],
                 providers: [
-                    { provide: UploadDataService, useValue: testUploadDataService }
+                    { provide: UploadDataService, useValue: new TestUploadDataService() }
                 ]
             }).compileComponents();
         }));
@@ -225,6 +256,87 @@ describe('app/polar/upload/file-preview.component', () => {
             });
         });
 
+        describe('onChangeSelection(event: MatCheckboxChange)', () => {
+            describe('called method directly', () => {
+                let updateStub: sinon.SinonStub;
+                let uploadService: UploadDataService;
+                beforeEach(() => {
+                    uploadService = fixture.debugElement.injector.get(UploadDataService);
+                    updateStub = sandbox.stub(uploadService, 'update');
+                });
+                testFiles.forEach((testFile: UploadFile) => {
+                    [true, false].forEach((checkedValue) => {
+                        it('should check ' + JSON.stringify(testFile) + ' as ' + (checkedValue ? 'selected' : 'not selected'), () => {
+                            // Needs cloning as objects get modified
+                            const testObj: UploadFile = Object.assign({}, testFile);
+                            (<any>cmpInstance).mUploadFile = testObj;
+                            cmpInstance.onChangeSelection(<any>{
+                                checked: checkedValue
+                            });
+                            expect(updateStub.callCount).toEqual(1);
+                            expect(testObj.selected).toEqual(checkedValue);
+                            expect((<any>cmpInstance).mUploadFile.selected).toEqual(checkedValue);
+                        });
+                    });
+                });
+            });
+            describe('called from layout event', () => {
+                let onChangeSelectionStub: sinon.SinonStub;
+                let slideToggleCmp: TestSlideToggleComponent;
+                beforeEach(() => {
+                    onChangeSelectionStub = sandbox.stub(cmpInstance, 'onChangeSelection');
+                    slideToggleCmp = fixture.debugElement.query(By.directive(TestSlideToggleComponent)).componentInstance;
+                });
+                testFiles.forEach((testFile: UploadFile) => {
+                    it('should call onChangeSelection method on component when clicked ' + testFile.valid, fakeAsync(() => {
+                        (<any>cmpInstance).mUploadFile = testFile;
+                        const testEvent: any = {
+                            checked: testFile.selected
+                        };
+                        slideToggleCmp.change.next(testEvent);
+                        fixture.detectChanges();
+                        expect(onChangeSelectionStub.callCount).toEqual(1, 'The event should be called once');
+                        expect(onChangeSelectionStub.getCall(0).args[0]).toEqual(testEvent);
+                    }));
+                });
+            });
+        });
 
+        describe('Layout', () => {
+            describe('after upload file was set', () => {
+                let slideToggleCmp: TestSlideToggleComponent;
+                let fileSizeSpan: HTMLElement;
+                let filenameSpan: HTMLElement;
+                beforeEach(() => {
+                    slideToggleCmp = fixture.debugElement.query(By.directive(TestSlideToggleComponent)).componentInstance;
+                    fileSizeSpan = fixture.debugElement.query(By.css('span.filesize')).nativeElement;
+                    filenameSpan = fixture.debugElement.query(By.css('h3.filename')).nativeElement;
+                });
+                testFiles.forEach((testFile: UploadFile) => {
+                    it('should update the toggle properties correctly', () => {
+                        (<any>cmpInstance).mUploadFile = testFile;
+                        fixture.detectChanges();
+                        expect(slideToggleCmp.disabled).toEqual(!testFile.valid,
+                            'The toggle should be ' + (testFile.valid ? 'enabled' : 'disabled'));
+                        expect(slideToggleCmp.checked).toEqual(testFile.selected && testFile.valid,
+                            'The toggle should be ' + ((testFile.selected && testFile.valid) ? 'selected' : 'not selected'));
+                    });
+                    const correctFileSize: number = (testFile.data ? testFile.data.length : 0);
+                    it('should update the displayed filesize to ' + correctFileSize, () => {
+                        (<any>cmpInstance).mUploadFile = testFile;
+                        fixture.detectChanges();
+                        expect(fileSizeSpan.innerText).toEqual('' + correctFileSize);
+                    });
+                    const correctFilename: string = testFile.filename ? testFile.filename : 'Unknown';
+                    it('should display the correct filename "' + correctFilename + '"', () => {
+                        (<any>cmpInstance).mUploadFile = testFile;
+                        fixture.detectChanges();
+                        expect(filenameSpan.innerText).toEqual(correctFilename);
+                    });
+
+                    it('should update the displayed errors correctly');
+                });
+            });
+        });
     });
 });
